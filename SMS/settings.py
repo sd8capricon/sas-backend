964b74058b199017fe15ebb1deb6a1c1454783df
sd8capricon <sd8capricon@gmail.com>
2022-03-18 22:09:20 +0530

    static files

--- SMS/settings.py
+++ SMS/settings.py
@@ -141,3 +141,6 @@ EMAIL_PORT = '587'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
