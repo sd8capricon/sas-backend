c5a948aca155ba24561760dc129a8fbc7c19a17a
sd8capricon <sd8capricon@gmail.com>
2022-03-20 18:40:33 +0530

    atten route fixes

--- api/controllers/attendance_controller.py
+++ api/controllers/attendance_controller.py
@@ -29,28 +29,34 @@ def attendance(req, courseId, lec_no):
         no_of_students = Student.objects.count()
         students_present = no_of_students
         absent_roll_nos = []
-        for student in students:
-            roll_no = student['student']
-            absent_roll_nos.append(roll_no)        
-        try:
-            course = Course.objects.get(pk=courseId)
-            for roll_no in range(1, no_of_students+1):
-                s = Student.objects.get(pk=roll_no)
-                attendance = Attendance(lec_no=lec_no, student=s, course=course)
-                if roll_no in absent_roll_nos:
-                    attendance.student_status=False
-                    students_present-=1
-                attendance.save()
-        except Exception as e:
-            error = {'error': str(e)}
+        course = Course.objects.get(pk=courseId)
+        lec = Lec_Stat.objects.filter(course=course, lec_no=lec_no)
+        if lec.count() == 0:
+            for student in students:
+                roll_no = student['student']
+                absent_roll_nos.append(roll_no)        
+            try:
+                course = Course.objects.get(pk=courseId)
+                for roll_no in range(1, no_of_students+1):
+                    s = Student.objects.get(pk=roll_no)
+                    attendance = Attendance(lec_no=lec_no, student=s, course=course)
+                    if roll_no in absent_roll_nos:
+                        attendance.student_status=False
+                        students_present-=1
+                    attendance.save()
+            except Exception as e:
+                error = {'error': str(e)}
+                return error
+            percentage = (students_present/no_of_students) * 100
+            stat = Lec_Stat(course=course, lec_no=lec_no, students_present=students_present, attendance_percentage=percentage)
+            stat.save()
+            statSerializer = StatSerializer(stat)
+            statcpy = statSerializer.data
+            statcpy['class_strength'] = no_of_students
+            return statcpy
+        else:
+            error = {'error': 'Lecture Already Marked'}
             return error
-        percentage = (students_present/no_of_students) * 100
-        stat = Lec_Stat(course=course, lec_no=lec_no, students_present=students_present, attendance_percentage=percentage)
-        stat.save()
-        statSerializer = StatSerializer(stat)
-        statcpy = statSerializer.data
-        statcpy['class_strength'] = no_of_students
-        return statcpy
     elif req.method == 'PATCH':
         students = req.data
         course = Course.objects.get(pk=courseId)
@@ -60,12 +66,15 @@ def attendance(req, courseId, lec_no):
             student_id = student['student']
             s = Student.objects.get(pk=student_id)
             attendance = Attendance.objects.get(lec_no=lec_no, student=s, course=course)
+            print(student['student_status'])
+            print(attendance.student_status)
             if student['student_status'] != attendance.student_status: # Check if incoming status is different
                 if student['student_status'] == True: # Incoming change is true ie student present then increment
                     students_present+=1 
                 else: # Else incoming change is false ie student absent then decrement
                     students_present-=1
                 attendance.student_status = student['student_status']
+                attendance.save()
         no_of_students = Student.objects.count()
         percentage = (students_present/no_of_students) * 100
         stat.students_present = students_present
@@ -81,7 +90,6 @@ def get_last_lecnum(req, course_id):
         try:
             lecs = Lec_Stat.objects.filter(course=course_id).aggregate(max=Max('lec_no'))
             lec_no = lecs['max']
-            print(lec_no)
             if lec_no == None:
                 lec_no = 0
             return {
